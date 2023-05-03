e0310df67e64cdebdda1caa32ed407826a93e5d6
facumor4 <facumora3103@gmail.com>
2023-05-03 15:42:59 -0300

    Funcion cuadratica

--- phyton/Trabajo.py
+++ phyton/Trabajo.py
@@ -4,3 +4,10 @@ f(x)= mx + b o y=mx + b
 def Funcion_lineal(m, x, b):
     y = m * x + b
     return y
+
+''' La funcion Cuadratica en simbolo es:
+f(x)= ax² + bx + c o y = ax² + bx + c
+'''
+def Funcion_Cuadratica(a,x,b,c):
+    y = a * x**2 + b * x + c
+    return y
