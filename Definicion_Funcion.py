ec48d95f3cd83e1e70c92cf6f573247907efd5c9
facumor4 <facumora3103@gmail.com>
2023-05-09 14:06:21 -0300

    Definicion de Proceso

--- Definicion_Funcion.py
+++
@@ -1,8 +0,0 @@
-''' 
-1) Escribir la definicion de función:
-Una función es una regla de correspondencia entre dos conjuntos
- de tal manera que a cada elemento del primer conjunto le corresponde uno y sólo un elemento del segundo conjunto.
-En lógica, una función es una expresión que toma uno o más valores de entrada, llamados argumentos,
- y produce un valor de salida, llamado resultado. Las funciones en lógica se utilizan para definir
-   proposiciones o para combinar proposiciones para construir argumentos más complejos.
-'''
---
+++ Definicion_Proceso.py
@@ -0,0 +1,5 @@
+'''
+2) Escribir la definición de proceso:
+En matemáticas, un proceso es una sucesión de operaciones o pasos lógicos que se realizan para llegar a una conclusión o solución.
+Estos procesos pueden ser numéricos o algebraicos, y pueden implicar la manipulación de variables, constantes o símbolos matemáticos.
+'''
