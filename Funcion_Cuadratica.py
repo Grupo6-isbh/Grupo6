93b43b78df408399d168505ef4ad62c8673e0e58
facumor4 <facumora3103@gmail.com>
2023-05-09 14:29:31 -0300

    Subiendo Funcion cuadratica

---
+++ Funcion_Cuadratica.py
@@ -0,0 +1,10 @@
+''' 
+5) Escribir en simbolos una función cuadrática, y su correspondiente codificación en python:
+La funcion Cuadratica en simbolo es:
+    f(x)= ax² + bx + c o y = ax² + bx + c 
+y = a * x^2 + b * x + c
+'''
+
+def Funcion_Cuadratica(a,x,b,c):
+    y = a * x**2 + b * x + c
+    return y
--- Funcion_Lineal.py
+++
@@ -1,9 +0,0 @@
-'''
-4) Escribir en simbolos una función lineal, y su correspondiente codificación en python:
-La funcion Lineal en simbolo es:
-    f(x)= mx + b o y=mx + b
-'''
-
-def Funcion_lineal(m, x, b):
-    y = m * x + b
-    return y
