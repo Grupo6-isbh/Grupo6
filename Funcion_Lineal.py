1781f1a4b7b0efb498f19d4fbedcbe20074f985a
facumor4 <facumora3103@gmail.com>
2023-05-09 14:26:57 -0300

    Subiendo funcion lineal

---
+++ Funcion_Lineal.py
@@ -0,0 +1,9 @@
+'''
+4) Escribir en simbolos una función lineal, y su correspondiente codificación en python:
+La funcion Lineal en simbolo es:
+    f(x)= mx + b o y=mx + b
+'''
+
+def Funcion_lineal(m, x, b):
+    y = m * x + b
+    return y
