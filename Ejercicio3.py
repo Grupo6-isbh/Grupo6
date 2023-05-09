cb6f58877502dd16e306eddff9b65865f8692edf
facumor4 <facumora3103@gmail.com>
2023-05-09 14:08:26 -0300

    Subiendo Ejercicio tres

---
+++ Ejercicio3.py
@@ -0,0 +1,15 @@
+'''
+3) Dado 2 conjuntos A y B, ¿cuando se corresponden con una función y cuando no? Dar ejemplos:
+Para que se corresponda una Función(f) tiene que tener un elemento único de relación entre el conjunto A y el conjunto B.
+Por ejemplo: si A es el conjunto {1, 2, 3} y B es el conjunto {a, b, c}, entonces una función f que asigna cada elemento
+de A a un elemento de B.
+f(1): a
+f(2): d
+f(3): c
+En cambio no podría funcionar si se repite, es decir si A es el conjunto {1,2,3} y B es el conjunto {a,b,c,d}.
+En el ejemplo que vemos a continuación, vamos a ver que el conjunto de A, se repite el número 1.
+f(1): a
+f(2): b
+f(3): c
+f(1): d
+'''
