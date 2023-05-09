2c20f6f7c54601e66a853080b9bdfa9370d092b8
facumor4 <facumora3103@gmail.com>
2023-05-08 21:34:56 -0300

    Suma primer intento

---
+++ Suma.py
@@ -0,0 +1,8 @@
+def suma():
+    num1=float(input("Ingrese un numero: "))
+    num2=float(input("Ingrese un numero: "))
+    suma=num1+num2
+    return suma
+
+resultado= suma()
+print("El resultado es: ",resultado)
